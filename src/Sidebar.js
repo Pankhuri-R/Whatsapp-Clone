e634935b0cfde1aa7910c52ee56c5e050f775444
Pankhuri-R <63069103+Pankhuri-R@users.noreply.github.com>
2021-10-02 09:22:15 +0530

    Building sidebar component

---
+++ src/Sidebar.js
@@ -0,0 +1,41 @@
+import React from "react";
+import "./Sidebar.css";
+import ChatIcon from "@mui/icons-material/Chat";
+import DonutLargeIcon from "@mui/icons-material/DonutLarge";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import { Avatar, IconButton } from "@material-ui/core";
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+import SidebarChat from "./SidebarChat";
+
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      <div className="sidebar__header">
+        <Avatar src="https://pbs.twimg.com/profile_images/1220044684791308288/xGeuSMdZ.jpg" />
+        <div className="sidebar__headerRight">
+          <IconButton>
+            <DonutLargeIcon />
+          </IconButton>
+          <IconButton>
+            <ChatIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </div>
+      </div>
+      <div className="sidebar__search">
+        <div className="sidebar__searchContainer">
+          <SearchOutlinedIcon />
+          <input type="text" placeholder="Search or start new chat" />
+        </div>
+      </div>
+      <div className="sidebar__chats">
+        <SidebarChat />
+        <SidebarChat />
+      </div>
+    </div>
+  );
+}
+
+export default Sidebar;
