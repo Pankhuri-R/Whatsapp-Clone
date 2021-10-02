4cd2f7575ae01e16303cd7a4d5a8b5436d02f6c3
Pankhuri-R <63069103+Pankhuri-R@users.noreply.github.com>
2021-10-02 09:21:37 +0530

    Starting frontend

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyB3dj2RwLSh9xlIFZlppdx5l3b6u8XgusY",
+  authDomain: "whatsapp-mern-1005.firebaseapp.com",
+  projectId: "whatsapp-mern-1005",
+  storageBucket: "whatsapp-mern-1005.appspot.com",
+  messagingSenderId: "49033194115",
+  appId: "1:49033194115:web:80ba8179448531de5049b6",
+};
